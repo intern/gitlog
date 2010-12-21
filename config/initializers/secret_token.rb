c038c57fe5437255827e94c6ad3d6bd08400ddd9
intern <lan_chi@foxmail.com>
2010-12-21 12:14:47 +0800

    First commit rails for gitlog rudiment.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitlog::Application.config.secret_token = '25a5ef81421161330fa9ab75f89f1d3410f3c556d0179c8a538652c1498642f1af060ed229dd49176a8b25fff6e1d7b91eaef773d017e60471dee3d6837ee0fd'
