c784464053167d0be314bd1985d1272d866861b6
Intern <lan_chi@qq.com>
2011-01-12 18:30:08 +0800

    Something.

--- app/models/git_picker.rb
+++ app/models/git_picker.rb
@@ -1,6 +1,6 @@
 class GitPicker < GitDrive::Picker
   git_config_with do |c|
     c.set_git_bin_path = '/usr/bin/git'
-    c.set_git_repositories_root = "/home/intern/repositories"
+    c.set_git_repositories_root = "/home/for_ssh/repositories"
   end
 end
